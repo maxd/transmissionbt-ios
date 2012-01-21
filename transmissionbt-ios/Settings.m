31ebb573b30abcdd912f291248dc8d62669bf03f
Maxim Dobryakov <closer.main@gmail.com>
2012-01-21 13:55:24 +0300

    Set default values for settings (because NSUserDefault doesn't support default values correctly)

--- transmissionbt-ios/Settings.m
+++ transmissionbt-ios/Settings.m
@@ -11,19 +11,35 @@ static NSUserDefaults* userDefaults = nil;
 }
 
 + (NSString *)rpcServerName {
+#ifdef DEBUG
+    return @"192.168.1.100";
+#else
     return [userDefaults stringForKey:@"rpc_server_name"];
+#endif
 }
 
 + (int)rpcServerPort {
+#ifdef DEBUG
+    return 9091;
+#else
     return [userDefaults integerForKey:@"rpc_server_port"];
+#endif
 }
 
 + (NSString *)rpcServerLogin {
+#ifdef DEBUG
+    return @"transmission";
+#else
     return [userDefaults stringForKey:@"rpc_server_login"];
+#endif
 }
 
 + (NSString *)rpcServerPassword {
+#ifdef DEBUG
+    return @"transmission";
+#else
     return [userDefaults stringForKey:@"rpc_server_password"];
+#endif
 }
 
 @end
