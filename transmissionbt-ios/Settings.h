841fb9fc12758a14e9644d88cb5430300cc9e0f1
Maxim Dobryakov <closer.main@gmail.com>
2012-01-21 10:32:07 +0300

    Add standard settings

---
+++ transmissionbt-ios/Settings.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+
+@interface Settings : NSObject
+
++ (NSString *)rpcServerName;
++ (NSString *)rpcServerPort;
++ (NSString *)rpcServerLogin;
++ (NSString *)rpcServerPassword;
+
+@end
---
+++ transmissionbt-ios/Settings.m
@@ -0,0 +1,29 @@
+#import "Settings.h"
+
+@implementation Settings
+
+static NSUserDefaults* userDefaults = nil;
+
++ (void)initialize {
+    [super initialize];
+
+    userDefaults = [NSUserDefaults standardUserDefaults];
+}
+
++ (NSString *)rpcServerName {
+    return [userDefaults stringForKey:@"rpc_server_name"];
+}
+
++ (NSString *)rpcServerPort {
+    return [userDefaults stringForKey:@"rpc_server_port"];
+}
+
++ (NSString *)rpcServerLogin {
+    return [userDefaults stringForKey:@"rpc_server_login"];
+}
+
++ (NSString *)rpcServerPassword {
+    return [userDefaults stringForKey:@"rpc_server_password"];
+}
+
+@end
